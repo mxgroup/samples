b3a74735fd8ef3277d2dfe0647452baab5fbb25e
Dmitry <shnipov@gmail.com>
2017-01-19 11:38:32 +0300

    Create CheckCostInsuranceOfReturn.sql

---
+++ SQL/Supplier.Returns/Tests/CheckCostInsuranceOfReturn.sql
@@ -0,0 +1,15 @@
+-- Поиск
+select
+	o.*
+from
+	PriorOrdersDetails pod
+	join OrdersDetails od on pod.OrderDetailId = od.OrderDetailId
+	join Orders o on o.OrderId = od.OrderId
+where
+	GlobalId = 110692278
+  
+-- Обновление
+begin tran
+update Orders set CostInsuranceOfReturn = 0 where OrderId = 4421886
+rollback
+commit
